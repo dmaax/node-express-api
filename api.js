18e7f30e77e66650a0e3a7554e22534e1c2b43d7
dmaax <dudzcool@gmail.com>
2021-07-01 19:21:48 -0300

    Update file structure

--- api.js
+++
@@ -1,7 +0,0 @@
-const ip = (req, res) => {
-    let key = req.query.key;
-    let ip = req.connection.remoteAddress;
-    return ip;
-};
-
-module.exports = { ip };
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const API = require('./api.js');
+const API = require('./middleware/apikeys');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/v1', (req, res) => {
-    res.send({status: 'OK!', IP: req.ip});
+app.get('/v1', API.validateKey, (req, res) => {
+    let key = req.query.key;
+    let host = req.hostname;
+    if (key) {
+        res.send({
+            status: 'OK!',
+            IP: req.ip,
+            Host: host,
+            Key: key
+        });
+        return;
+    }
+    res.send({
+        status: 'OK!',
+        Key: "Nenhuma chave",
+        Host: host
+    });
 });
 
 app.listen(port, () => {
